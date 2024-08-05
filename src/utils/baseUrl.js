8450c3082a7ee72ea912fe4244182fd30b1546f4
ClarenceG01 <gatama001@gmail.com>
2024-08-05 14:54:27 +0300

    icon, baseurl

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,6 @@
 function baseUrl() {
   // return "https://expense-tracker-server-xdkv.onrender.com";
-  return "http://localhost:9000";
+  // return "http://localhost:9000";
+  return "https://expense-tracker-server-xdkv.onrender.com";
 }
 export default baseUrl;
