b2017fef769ab855f00664f4369c7f3bc5b9800b
ClarenceG01 <gatama001@gmail.com>
2024-08-13 07:38:17 +0300

    use usenavigate in privateroute

--- src/components/authentication/PrivateRoutes.jsx
+++ src/components/authentication/PrivateRoutes.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import { checkAuth } from "../../utils/checkAuth";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const PrivateRoutes = () => {
+  const navigate = useNavigate();
   const isToken = checkAuth();
-  return isToken ? <Outlet /> : <Navigate to="/" />;
+
+  useEffect(() => {
+    if (!isToken) {
+      navigate("/");
+    }
+  }, [isToken, navigate]);
+  return isToken ? <Outlet /> : null;
 };
 
 export default PrivateRoutes;
